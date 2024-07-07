e30d370c1fe3e8ffb39a869e5a3c36ebd4eec323
connorlagana <connor.lagana@gmail.com>
2024-07-07 17:23:21 -0400

    Initial Commit

---
+++ MinuteMove/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  MinuteMove
+//
+//  Created by Coding on 7/7/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
